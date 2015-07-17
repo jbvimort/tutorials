75bda85930aaa0cd5ed3c208ab8fbda404b9fdb3
Jean-Baptiste VIMORT <jb.vimort@gmail.com>
2015-07-17 14:45:59 -0400

    ShrinkFilter (working)

--- python/ShrinkFilter.py
+++ python/ShrinkFilter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import sys
 import SimpleITK as sitk
-import numpy as np
 
 
 if len(sys.argv) != 2:
@@ -13,25 +12,19 @@ a=10
 b=10
 
 ### METHOD 1
+print "method 1"
 output1 = sitk.Shrink(input,[a,b])
 sitk.WriteImage(sitk.Cast(output1,input.GetPixelID()), 'method1.jpg')
 
-### METHOD 2 : selection of pixels -> not working
-print str(input.GetPixelID())
-output2 = sitk.Image(int(input.GetWidth()/a),int(input.GetHeight()/b),input.GetPixelID())
-print input.GetPixelIDTypeAsString()
-
+### METHOD 2 : selection of pixels
+print "method 2"
+output2 = sitk.Image([int(input.GetWidth()/a),int(input.GetHeight()/b)],input.GetPixelID(),3)
 
 # Application of Shrink Filter
 k=0
 l=0
 for i in range(1,input.GetWidth()-1,a):
     for j in range(1,input.GetHeight()-1,b):
-        print str(input.GetPixel(i,j))
-        print str(i)
-        print str(j)
-        #output2.SetPixelAsVectorUInt8(k,l,input.GetPixel(i,j))
-        #output2.SetPixel(k,l,input.GetPixel(i,j))
         output2[k,l]=input.GetPixel(i,j)
         l=l+1
     k=k+1
@@ -39,23 +32,49 @@ for i in range(1,input.GetWidth()-1,a):
         
 sitk.WriteImage(sitk.Cast(output2,input.GetPixelIDValue()), 'method2.jpg')
 
-#### METHOD 3 : mean of square -> not working
-output3 = sitk.Image(int(input.GetWidth()/a),int(input.GetHeight()/b),input.GetPixelID())
+#### METHOD 3 : mean of square
+print "method 3"
+output3 = sitk.Image([int(input.GetWidth()/a),int(input.GetHeight()/b)],input.GetPixelID(),3)
 
-# Application of Shrink Filter
-k=0
-l=0
+#Application of Shrink Filter
 SumShrink=0
-for k in range(a,output3.GetWidth()-a):
-    for l in range(b,output3.GetHeight()-b):
+for k in range(1,output3.GetWidth()):
+    for l in range(1,output3.GetHeight()):
         val=[0,0,0]
-        for i in range(k-a,k+a):
-            for j in range(l-b,l+b):
+        for i in range(1,a):
+            for j in range(1,b):
                 for c in range(0,3):
-                    val[c]=val[c]+input.GetPixel(i,j)[c] 
+                    val[c]=val[c]+input.GetPixel(i+a*(k-1),j+b*(l-1))[c]
         SumShrink=(int(val[0]/(a*b)),int(val[1]/(a*b)),int(val[2]/(a*b)))
-                 
         output3.SetPixel(k,l,SumShrink)
 
-        
+
 sitk.WriteImage(sitk.Cast(output3,input.GetPixelIDValue()), 'method3.jpg')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
