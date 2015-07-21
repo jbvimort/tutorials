6eac3b20f17987cf2298ca89eacf6c3d1387f1e8
Jean-Baptiste VIMORT <jb.vimort@gmail.com>
2015-07-21 17:15:07 -0400

    ITK erode folter (working)

---
+++ C++/erode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "itkConstNeighborhoodIterator.h"
+#include "itkImageRegionIterator.h"
+int main()
+{
+    std::string variable="Mon premier";
+    variable += " programme.";
+    std::cout<<variable<<std::endl;
+    return 0;
+}
---
+++ C++/erode/erode.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <string>
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkNeighborhoodIterator.h"
+#include "itkImageRegionIterator.h"
+#include "itkNeighborhoodAlgorithm.h"
+
+int main(int argc, char **argv )
+{
+    ///check of the number of informations given
+    /// ---------------------------------------------------------
+
+    if (argc != 2)
+    {
+        std::cout<< "Usage: "<<argv[0]<<" InputFileNAme";
+        exit(-1);
+    }
+
+    ///reading of the input file
+    /// --------------------------------
+
+    //definition of the data type
+    typedef unsigned char PixelType;
+    const unsigned int Dimension = 2;
+    typedef itk::Image< PixelType, Dimension > ImageType;
+    typedef itk::ImageFileReader< ImageType > ReaderType;
+
+    //definition of the reader pointer
+    ReaderType::Pointer reader = ReaderType::New();
+
+    //reading of the image
+    const char * filename = argv[1];
+    reader->SetFileName( filename );
+    reader->Update();
+
+    ///definition of the writer
+    ///--------------------------------
+
+    typedef  itk::ImageFileWriter< ImageType  > WriterType;
+    WriterType::Pointer writer = WriterType::New();
+
+    ///------------------------------------------------///
+    ///        erode filter  (methode 1)         ///
+    ///-----------------------------------------------///
+
+    //definition of the kernel's size
+    int R=5;
+
+    //definition of the filters
+    typedef itk::NeighborhoodIterator< ImageType > NeighborhoodIteratorType;
+    typedef itk::ImageRegionIterator< ImageType> IteratorType;
+
+    //creation of the input image iterator (neighborhood iterator)
+    NeighborhoodIteratorType::RadiusType radius;
+    radius.Fill(R);
+    NeighborhoodIteratorType it( radius, reader->GetOutput(),reader->GetOutput()->GetRequestedRegion() );
+
+    //creation of the output iterator
+    ImageType::Pointer output1 = ImageType::New();
+    output1->SetRegions(reader->GetOutput()->GetRequestedRegion());
+    output1->Allocate();
+    IteratorType out1(output1, reader->GetOutput()->GetRequestedRegion());
+
+    //creation of a vector of offset (those offset will constitute the kernel)
+    std::vector<NeighborhoodIteratorType::OffsetType> usefullOffset;
+    usefullOffset.resize(4*R+1);
+
+    //filling of the offset vector (here the kernel will be a cross)
+    NeighborhoodIteratorType::OffsetType temp = {{0,0}};
+    usefullOffset.at(0)=temp;
+    for(int i=1 , j=1 ; i<R ; i++ , j=j+4)
+    {
+        NeighborhoodIteratorType::OffsetType temp1={{-i,0}};
+        usefullOffset[j]=temp1;
+        NeighborhoodIteratorType::OffsetType temp2={{0,-i}};
+        usefullOffset[j+1]=temp2;
+        NeighborhoodIteratorType::OffsetType temp3={{i,0}};
+        usefullOffset[j+2]=temp3;
+        NeighborhoodIteratorType::OffsetType temp4={{0,i}};
+        usefullOffset[j+3]=temp4;
+    }
+
+    //preparation of the image by filling them with 0
+    for (out1.GoToBegin(); !out1.IsAtEnd(); ++out1)
+    {
+        out1.Set(0);
+    }
+
+    //erosion main algorythms
+    for (it.GoToBegin(), out1.GoToBegin(); !it.IsAtEnd(); ++it, ++out1)
+    {
+        for(int i=0;i<4*(R-1)+1;i++)
+        {
+
+            if(it.GetPixel(usefullOffset[i])!=0)
+            {
+                out1.Set(255);
+            }
+        }
+    }
+
+    ///writing of the output file
+    /// ---------------------------------
+
+    std::cout<<"methode 1"<<std::endl;
+
+    writer->SetFileName("method1.jpg");
+    writer->SetInput(output1);
+    writer->Update();
+
+
+
+    ///-------------------------------------------------///
+    ///        erode filter  (methode 2)          ///
+    ///------------------------------------------------///
+
+    //creation of the output iterator
+    ImageType::Pointer output2 = ImageType::New();
+    output2->SetRegions(reader->GetOutput()->GetRequestedRegion());
+    output2->Allocate();
+    NeighborhoodIteratorType out2(radius, output2, reader->GetOutput()->GetRequestedRegion());
+
+    //creation of a facelist to avoid the bounderies
+    typedef itk::NeighborhoodAlgorithm
+            ::ImageBoundaryFacesCalculator< ImageType > FaceCalculatorType;
+    FaceCalculatorType faceCalculator;
+    FaceCalculatorType::FaceListType faceList;
+
+    faceList = faceCalculator(reader->GetOutput(), output2->GetRequestedRegion(),out2.GetRadius());
+
+    FaceCalculatorType::FaceListType::iterator fit;
+
+
+    //preparation of the image by filling them with 0
+    for (out2.GoToBegin(); !out2.IsAtEnd();++out2)
+    {
+        out2.SetPixel(usefullOffset[0],0);
+    }
+
+    fit=faceList.begin();
+    it = NeighborhoodIteratorType( radius, reader->GetOutput(),*fit);
+    out2 =NeighborhoodIteratorType( radius,reader->GetOutput(), *fit );
+    //erosion main algorythms
+    for (it.GoToBegin(), out2.GoToBegin(); !it.IsAtEnd(); ++it, ++out2)
+    {
+        if(it.GetPixel(usefullOffset[0])!=0)
+        {
+            for(int i=0;i<4*(R-1)+1;i++)
+            {
+                std::cout<<usefullOffset[i]<<std::endl;
+                out2.SetPixel(usefullOffset[i],255);
+            }
+        }
+    }
+
+    ///writing of the output file
+    /// ---------------------------------
+
+    std::cout<<"methode 2"<<std::endl;
+
+    writer->SetFileName("method2.jpg");
+    writer->SetInput(output2);
+    writer->Update();
+}
